206cc4b29b43eba427b0a424bd08a8a9fdf9f34f
Adam Gutglick <adam@pinecone.io>
2022-09-06 18:56:41 +0300

    Update the alloc_trait feature to the newer Allocator trait
    Signed-off-by: Adam Gutglick <adam@pinecone.io>

--- jemallocator/tests/shrink_in_place.rs
+++ jemallocator/tests/shrink_in_place.rs
@@ -9,24 +9,28 @@ static A: Jemalloc = Jemalloc;
 #[cfg(feature = "alloc_trait")]
 fn shrink_in_place() {
     unsafe {
-        use std::alloc::{Alloc, Layout};
+        use core::alloc::{Allocator, Layout};
 
-        // allocate a "large" block of memory:
-        let orig_sz = 10 * 4096;
+        // allocate 7 bytes which end up in the 8 byte size-class as long as
+        // jemalloc's default size classes are used:
+        let orig_sz = 7;
         let orig_l = Layout::from_size_align(orig_sz, 1).unwrap();
-        let ptr = Jemalloc.alloc(orig_l).unwrap();
+        let ptr = Jemalloc.allocate(orig_l).unwrap();
+        let ptr = ptr.as_non_null_ptr();
 
-        // try to shrink it in place to 1 byte - if this succeeds,
-        // the size-class of the new allocation should be different
-        // than that of the original allocation:
-        let new_sz = 1;
-        if let Ok(()) = Jemalloc.shrink_in_place(ptr, orig_l, new_sz) {
-            // test that deallocating with the new layout succeeds:
-            let new_l = Layout::from_size_align(new_sz, 1).unwrap();
-            Jemalloc.dealloc(ptr, new_l);
-        } else {
-            // if shrink in place failed - deallocate with the old layout
-            Jemalloc.dealloc(ptr, orig_l);
-        }
+        // try to grow it in place by 1 byte - it should grow without problems:
+        let new_sz = orig_sz + 1;
+        let new_l = Layout::from_size_align(new_sz, 1).unwrap();
+        assert!(Jemalloc.grow(ptr, orig_l, new_l).is_ok());
+        let new_l = Layout::from_size_align(orig_sz + 1, 1).unwrap();
+
+        // trying to do it again fails because it would require moving the
+        // allocation to a different size class which jemalloc's xallocx does not
+        // do:
+        let new_sz = new_sz + 1;
+        let new_new_l = Layout::from_size_align(new_sz, 1).unwrap();
+        assert!(Jemalloc.grow(ptr, new_l, new_new_l).is_err());
+
+        Jemalloc.deallocate(ptr, new_l)
     }
 }
